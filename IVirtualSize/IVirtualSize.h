ae724c73949a57b468ed705e3c93b9588e9ba3ed
RenhardJH <renhard.hutajulu@koinworks.com>
2023-07-27 16:30:09 +0700

    Initial project setup

---
+++ IVirtualSize/IVirtualSize.h
@@ -0,0 +1,18 @@
+//
+//  IVirtualSize.h
+//  IVirtualSize
+//
+//  Created by RenhardJH on 27/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IVirtualSize.
+FOUNDATION_EXPORT double IVirtualSizeVersionNumber;
+
+//! Project version string for IVirtualSize.
+FOUNDATION_EXPORT const unsigned char IVirtualSizeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IVirtualSize/PublicHeader.h>
+
+
